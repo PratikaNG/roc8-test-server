b3da9f09cb3b148ee1b4f5b957af9e67a237e311
Pratika NG <pratika.05ng@gmail.com>
2023-08-20 12:33:14 +0530

    added session id

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.all("/", getToken, async (req, res) => {
         "Api-key": "4NKQ3-815C2-8T5Q2-16318-55301",
         "Auth-token": `${token}`,
       },
-      data: { session_id },
+      data:{"session_id":"eyJpdiI6IkVNUkZ1N0hlSHhHSnJ3Vjl4aUlxc0E9PSIsInZhbHVlIjoieFlxa1wvVDYxQWl5U2pxMDFcL0R6ZVVvdEN6Mkk0R29TRDN3ZnN0U3VGcER0cEFMa2NVb0xNcDJudjlRTHRUbGJkIiwibWFjIjoiMTE0MmU0MGE5YmJhMzY4Nzc4MDExNmZkNTI1MjZhMGE3OTQyMDZmOTc1MTVmZDM1Mzc3ZmJmNjhmMzllOGYxYSJ9"},
     })
       .then((result) => {
         res.json({ msg: "Success", data: result.data });
@@ -41,3 +41,6 @@ app.all("/", getToken, async (req, res) => {
 app.listen(8080, () => {
   console.log(`Server running at 8080`);
 });
+
+
+
